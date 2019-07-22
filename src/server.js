ad20ddcf3955b9eec2ad8030df9dd7f08c75d67f
Estelle K <e.keaton@live.com>
2019-07-22 15:05:54 -0700

    deploying to heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
-const app = require('./app')
-const { PORT } = require('./config')
+const express = require('express');
+const app = express();
 
-app.listen(PORT, () => {
+ const PORT = process.env.PORT || 8000;
+
+ app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
+
+ module.exports = {app};
